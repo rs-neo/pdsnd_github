aaa7eebffb1537e85f04197c1623ee2bcdbb592d
Riaan Swart <rs.subscriptions@gmail.com>
2021-04-08 07:30:28 +1000

    docs: Added function comments in Bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -4,12 +4,12 @@ def main():
     while True:
             city, month, day = get_filters()
             df = load_data(city, month, day)            
-                       
+            # External functions for statistical analytics           
             time_stats(df)
             station_stats(df)
             trip_duration_stats(df)
             user_stats(df)
-            
+            # External function, diplay 5 records at a time
             raw_data(df)
 
             restart = input('\nWould you like to restart? Enter yes or no.\n').lower().strip()
